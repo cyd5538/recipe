a1fb8de01bf92926a9e77376bcf502ebac9f8925
cyd5538 <cyd5538@naver.com>
2025-03-03 22:23:09 +0900

    Fix: login했을대 로그인,회원가입 페이지 접근 방지

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,19 @@
 
 import Logo from '@/components/layout/header/Logo';
 import { LoginForm } from '@/components/login/LoginForm';
+import { useAuthUser } from '@/hooks/useAuthUser';
+import { redirect } from 'next/navigation';
 
 const Login = () => {
+  const { user, loading } = useAuthUser();
+
+  if (loading) {
+    return <div className="flex items-center justify-center min-h-screen">Loading...</div>;
+  }
+
+  if (user) {
+    redirect("/");
+  }
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-white sm:bg-gray-100">
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,8 +2,20 @@
 
 import Logo from "@/components/layout/header/Logo";
 import { SignUpForm } from "@/components/signup/SignUpForm";
+import { useAuthUser } from "@/hooks/useAuthUser";
+import { redirect } from "next/navigation";
 
 const SignUpPage = () => {
+  const { user, loading } = useAuthUser();
+
+  if (loading) {
+    return <div className="flex items-center justify-center min-h-screen">Loading...</div>;
+  }
+
+  if(!user) {
+    redirect("/")
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-white sm:bg-gray-100">
       <div className="w-full sm:max-w-md bg-white p-8 rounded-lg shadow-none sm:shadow-md">
