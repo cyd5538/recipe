7ae0d20c364ce3c14850838eec4e2c88c3ffd75e
cyd5538 <cyd5538@naver.com>
2025-03-08 14:12:09 +0900

    style: 공용 loading 컴포넌트

---
+++ src/components/ui/loading.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
+interface Props {
+  className?: string;
+}
+
+const Loading: React.FC<Props> = ({ className = "" }) => {
+  return (
+    <div
+      className={`flex items-center justify-center inset-0 bg-white/50 backdrop-blur-sm ${className}`}
+    >
+      <AiOutlineLoading3Quarters size={28} className="animate-spin transition-all delay-75 text-gray-600" />
+    </div>
+  );
+};
+
+export default Loading;
