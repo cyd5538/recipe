4b81345e3a5301741720adb4dc5aa51429dcf051
cyd5538 <cyd5538@naver.com>
2025-03-08 19:12:22 +0900

    style: category, options 항목 수정

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,17 +1,17 @@
 export const recipeCategories = [
-    { value: "korean", label: "한식" },
-    { value: "western", label: "양식" },
-    { value: "chinese", label: "중식" },
-    { value: "japanese", label: "일식" },
-    { value: "dessert", label: "디저트" },
+    { value: "한식", label: "한식" },
+    { value: "양식", label: "양식" },
+    { value: "중식", label: "중식" },
+    { value: "일식", label: "일식" },
+    { value: "디저트", label: "디저트" },
   ];
   
   export const cookTimeCategories = [
-    { value: "15", label: "15분" },
-    { value: "30", label: "30분" },
-    { value: "60", label: "1시간" },
-    { value: "120", label: "2시간" },
-    { value: "180", label: "2시간 이상" },
+    { value: "15분", label: "15분" },
+    { value: "30분", label: "30분" },
+    { value: "1시간", label: "1시간" },
+    { value: "2시간", label: "2시간" },
+    { value: "2시간 이상", label: "2시간 이상" },
   ];
   
   export const difficultyCategories = [
@@ -21,10 +21,10 @@ export const recipeCategories = [
   ];
   
   export const materialPriceCategories = [
-    { value: "10000", label: "10,000원" },
-    { value: "20000", label: "20,000원" },
-    { value: "50000", label: "50,000원" },
-    { value: "100000", label: "100,000원" },
-    { value: "200000", label: "기타" },
+    { value: "10,000원", label: "10,000원" },
+    { value: "20,000원", label: "20,000원" },
+    { value: "50,000원", label: "50,000원" },
+    { value: "100,000원", label: "100,000원" },
+    { value: "100,000원 이상", label: "기타" },
   ];
   
--- src/constants/options.tsx
+++ src/constants/options.tsx
@@ -5,20 +5,20 @@ import { GiSushis } from "react-icons/gi";
 
 export const categoryOptions = [
     { value: "all", label: "All", icon: <AiOutlineGlobal /> },
-    { value: "korean", label: "한식", icon: <FaBowlRice /> },
-    { value: "chinese", label: "중식", icon: <ImFire /> },
-    { value: "japanese", label: "일식", icon: <GiSushis /> },
-    { value: "western", label: "양식", icon: <GiSushis /> },
-    { value: "dessert", label: "디저트", icon: <FaBreadSlice /> },
+    { value: "한식", label: "한식", icon: <FaBowlRice /> },
+    { value: "중식", label: "중식", icon: <ImFire /> },
+    { value: "일식", label: "일식", icon: <GiSushis /> },
+    { value: "양식", label: "양식", icon: <GiSushis /> },
+    { value: "디저트", label: "디저트", icon: <FaBreadSlice /> },
 ];
 
 export const timeOptions = [
     { value: "all", label: "All" },
-    { value: "15", label: "15분" },
-    { value: "30", label: "30분" },
-    { value: "60", label: "1시간" },
-    { value: "120", label: "2시간" },
-    { value: "180", label: "2시간 이상" },
+    { value: "15분", label: "15분" },
+    { value: "30분", label: "30분" },
+    { value: "1시간", label: "1시간" },
+    { value: "2시간", label: "2시간" },
+    { value: "2시간 이상", label: "2시간 이상" },
 ];
 
 export const difficultyOptions = [
@@ -28,9 +28,9 @@ export const difficultyOptions = [
 ];
 
 export const priceOptions = [
-    { value: "10000", label: "10,000원" },
-    { value: "20000", label: "20,000원" },
-    { value: "50000", label: "50,000원" },
-    { value: "100000", label: "100,000원" },
-    { value: "200000", label: "기타" },
+    { value: "10,000원", label: "10,000원" },
+    { value: "20,000원", label: "20,000원" },
+    { value: "50,000원", label: "50,000원" },
+    { value: "100,000원", label: "100,000원" },
+    { value: "기타", label: "기타" },
 ];
