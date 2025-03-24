c086f3cacea03b00088cdc80d7888682e49440f6
cyd5538 <cyd5538@naver.com>
2025-03-24 23:59:21 +0900

    Fix: require에서 import로 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,105 +1,105 @@
-import type { Config } from "tailwindcss";
+import { type Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 
-export default {
-	darkMode: ["class"],
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			screens: {
-				'1125px': '1125px',
-			},
-			colors: {
-				background: 'hsl(var(--background))',
-				foreground: 'hsl(var(--foreground))',
-				card: {
-					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))'
-				},
-				popover: {
-					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))'
-				},
-				primary: {
-					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))'
-				},
-				secondary: {
-					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))'
-				},
-				muted: {
-					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))'
-				},
-				accent: {
-					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))'
-				},
-				destructive: {
-					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))'
-				},
-				border: 'hsl(var(--border))',
-				input: 'hsl(var(--input))',
-				ring: 'hsl(var(--ring))',
-				chart: {
-					'1': 'hsl(var(--chart-1))',
-					'2': 'hsl(var(--chart-2))',
-					'3': 'hsl(var(--chart-3))',
-					'4': 'hsl(var(--chart-4))',
-					'5': 'hsl(var(--chart-5))'
-				}
-			},
-			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
-			},
-			// 기본 HTML 제목 스타일 추가
-			typography: {
-				DEFAULT: {
-					css: {
-						h1: {
-							fontSize: '1.5em',
-							fontWeight: '700',
-							marginTop: "10",
-							marginBottom: "10"
-						},
-						h2: {
-							fontSize: '1.25em',
-							fontWeight: '700',
-							marginTop: "5",
-							marginBottom: "5"
-						},
-						h3: {
-							fontSize: '1.1em',
-							fontWeight: '700',
-							marginTop: "2",
-							marginBottom: "2"
-						},
-						p: {
-							marginTop: "0",
-							marginBottom: "0"
-						},
-						li: {
-							marginTop: "2",
-							marginBottom: "2"
-						},
-						ol: {
-							marginTop: "2",
-							marginBottom: "2"
-						}
-					},
-				},
-			},
-		}
-	},
-	plugins: [
-		require("tailwindcss-animate"),
-		require('@tailwindcss/typography')
-	],
-} satisfies Config;
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        "1125px": "1125px",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              fontSize: "1.5em",
+              fontWeight: "700",
+              marginTop: "10",
+              marginBottom: "10",
+            },
+            h2: {
+              fontSize: "1.25em",
+              fontWeight: "700",
+              marginTop: "5",
+              marginBottom: "5",
+            },
+            h3: {
+              fontSize: "1.1em",
+              fontWeight: "700",
+              marginTop: "2",
+              marginBottom: "2",
+            },
+            p: {
+              marginTop: "0",
+              marginBottom: "0",
+            },
+            li: {
+              marginTop: "2",
+              marginBottom: "2",
+            },
+            ol: {
+              marginTop: "2",
+              marginBottom: "2",
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate, typography],
+};
+
+export default config;
