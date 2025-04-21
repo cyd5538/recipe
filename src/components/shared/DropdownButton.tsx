76ec98c34040a9b873678da09471da9f1778cf0b
cyd5538 <cyd5538@naver.com>
2025-04-21 19:32:09 +0900

    style: DropdownButton 컴포넌트에서 font-semibold 추가

--- src/components/shared/DropdownButton.tsx
+++ src/components/shared/DropdownButton.tsx
@@ -10,7 +10,7 @@ interface DropdownButtonProps {
 const DropdownButton: React.FC<DropdownButtonProps> = ({ label, selectedValue, onToggle, variant = "default" }) => {
   return (
     <button
-      className={`w-full cursor-pointer bg-red-400 dark:bg-zinc-800 text-white mt-10 px-2 py-2 mb-4 sm:text-lg rounded-xl  transition-all duration-200 shadow-md text-center 
+      className={`w-full cursor-pointer bg-red-400 dark:bg-zinc-800 font-semibold mt-10 px-2 py-2 mb-4 sm:text-lg rounded-xl  transition-all duration-200 shadow-md text-center 
       `}
       onClick={onToggle}
     >
